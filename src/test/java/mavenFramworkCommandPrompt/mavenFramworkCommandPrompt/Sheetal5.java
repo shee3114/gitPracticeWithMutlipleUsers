5bbc3e7301d1e5d29629116c17b7514a203c6252
shee3114 <sheetaldesai3114@gmail.com>
2020-09-01 11:26:54 +0530

    commit by user X

---
+++ src/test/java/mavenFramworkCommandPrompt/mavenFramworkCommandPrompt/Sheetal5.java
@@ -0,0 +1,16 @@
+package mavenFramworkCommandPrompt.mavenFramworkCommandPrompt;
+
+import org.testng.annotations.Test;
+
+public class Sheetal5 {
+	@Test
+	public void testNine() {
+		System.out.println("---Nine Test Case");
+	}
+
+	@Test
+	public void testTen() {
+		System.out.println("---Ten test case");
+		System.out.println("important");
+	}
+}
