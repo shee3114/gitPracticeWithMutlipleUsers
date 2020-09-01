02d4400fa3e4a7dedbb950542a10857253a14f8f
shee3114 <sheetaldesai3114@gmail.com>
2020-09-02 01:02:31 +0530

    Latest code is pushed by User Y at 1:02 am

--- src/test/java/mavenFramworkCommandPrompt/mavenFramworkCommandPrompt/SheetalTest2.java
+++ src/test/java/mavenFramworkCommandPrompt/mavenFramworkCommandPrompt/SheetalTest2.java
@@ -3,13 +3,18 @@ package mavenFramworkCommandPrompt.mavenFramworkCommandPrompt;
 import org.testng.annotations.Test;
 
 public class SheetalTest2 {
- @Test
- public void testThree(){
-	 System.out.println("---Third Test Case");
- }
- @Test
- public void testFour(){
-	 System.out.println("---Forth test case");
- }
+	@Test
+	public void testThree() {
+		System.out.println("---Third Test Case");
+	}
 
+	@Test
+	public void testFour() {
+		System.out.println("---Forth test case");
+	}
+
+	@Test
+	public void codeWrittenByUserY() {
+		System.out.println(" code written by user Y at 1 am.");
+	}
 }
