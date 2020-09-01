06398d099631b8104c3e3de4bd2c6429daf947b9
shee3114 <sheetaldesai3114@gmail.com>
2020-09-01 11:48:30 +0530

    second commit by user X

---
+++ src/test/java/mavenFramworkCommandPrompt/mavenFramworkCommandPrompt/Sheetal6.java
@@ -0,0 +1,17 @@
+package mavenFramworkCommandPrompt.mavenFramworkCommandPrompt;
+
+import org.testng.annotations.Test;
+
+public class Sheetal6 {
+	@Test
+	public void testNine() {
+		System.out.println("---Eleven Test Case");
+	}
+
+	@Test
+	public void testTen() {
+		System.out.println("---Twelve test case");
+
+	}
+
+}
