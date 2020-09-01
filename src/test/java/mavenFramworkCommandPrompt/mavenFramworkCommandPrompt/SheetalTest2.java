47b3e35d98dda63d5bd746d4e7da90cf5c4c92fb
shee3114 <sheetaldesai3114@gmail.com>
2020-09-02 00:48:02 +0530

    forth commit by User X

--- src/test/java/mavenFramworkCommandPrompt/mavenFramworkCommandPrompt/SheetalTest2.java
+++ src/test/java/mavenFramworkCommandPrompt/mavenFramworkCommandPrompt/SheetalTest2.java
@@ -3,12 +3,20 @@ package mavenFramworkCommandPrompt.mavenFramworkCommandPrompt;
 import org.testng.annotations.Test;
 
 public class SheetalTest2 {
- @Test
- public void testThree(){
-	 System.out.println("---Third Test Case");
- }
- @Test
- public void testFour(){
-	 System.out.println("---Forth test case");
- }
+	@Test
+	public void testThree() {
+		System.out.println("---Third Test Case");
+	}
+
+	@Test
+	public void testFour() {
+		System.out.println("---Forth test case");
+	}
+
+	@Test
+	public void codeWrittenByXUser() {
+		System.out.println("Code written By X User");
+	}
+	
+	
 }
