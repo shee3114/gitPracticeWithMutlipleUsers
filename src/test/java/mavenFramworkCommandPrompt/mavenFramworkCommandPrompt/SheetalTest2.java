9312f04a8e9b8ecaabec67084d889bfa173ee5ee
shee3114 <sheetaldesai3114@gmail.com>
2020-09-03 00:12:47 +0530

    commit

--- src/test/java/mavenFramworkCommandPrompt/mavenFramworkCommandPrompt/SheetalTest2.java
+++ src/test/java/mavenFramworkCommandPrompt/mavenFramworkCommandPrompt/SheetalTest2.java
@@ -13,12 +13,8 @@ public class SheetalTest2 {
 	public void testFour() {
 		System.out.println("---Forth test case");
 	}
-
-<<<<<<< HEAD
-	@Test
+@Test
 	public void CodeWrittenByUserY() {
 		System.out.println("Code written by UserY");
 	}
-=======
->>>>>>> 22c62fc6f5bc51c05ed86c2b76f232f7cadef518
 }
